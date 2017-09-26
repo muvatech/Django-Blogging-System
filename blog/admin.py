635f22d4224e2b774e9b139613271bb0c19a173c
henrymbuguak <henrymbuguak@gmail.com>
2017-09-26 12:50:59 +0300

    Admin site customization

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','author','status','created')
+    list_filter = ('status','created','publish','author')
+    search_fields = ('title','author')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+admin.site.register(Post, PostAdmin)
